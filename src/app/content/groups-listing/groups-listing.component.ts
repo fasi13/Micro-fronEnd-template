ccece498e1483bc66e0921978eaf94793ea644fa
Kenji Lozano <kenji.lozano@hinda.com>
2018-10-16 16:12:42 -0400

    [FGE-169] Implemented content groups listing page

--- src/app/content/groups-listing/groups-listing.component.ts
+++ src/app/content/groups-listing/groups-listing.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { State, getGroups, ApplicationContent, FgeRouterService } from '@forge/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'fge-groups-listing',
   templateUrl: './groups-listing.component.html',
-  styleUrls: ['./groups-listing.component.scss']
 })
 export class GroupsListingComponent implements OnInit {
 
-  constructor() { }
+  groups$: Observable<ApplicationContent[]>;
+
+  constructor(
+    private store: Store<State>,
+    private fgeRouter: FgeRouterService
+  ) { }
 
   ngOnInit() {
+    this.initSelectors();
+  }
+
+  goToContentGroup(groupId: string): void {
+    this.fgeRouter.navigate(`content/group/${groupId}`);
+  }
+
+  private initSelectors() {
+    this.groups$ = this.store.select(getGroups);
   }
 
 }
