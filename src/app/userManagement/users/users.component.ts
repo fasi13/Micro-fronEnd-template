690c92d2c3a7316187cf61d478ebad313fc69098
Daniel Canqui <dcanqui@hinda.com>
2018-10-22 17:23:45 -0400

    [FGE-171] Created a shared folder to contain user form modal component.

---
+++ src/app/userManagement/users/users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'fge-users',
+    templateUrl: './users.component.html'
+})
+
+export class UsersComponent implements OnInit {
+    ngOnInit() {
+    }
+
+    openCreateUserModal(): void {
+
+    }
+}
