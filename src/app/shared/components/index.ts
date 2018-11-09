98dc5c227c2fe36e6cf882c4e5b050113f1a6071
Rodrigo Mendoza <rmendoza@hinda.com>
2018-11-09 09:52:23 -0400

    [FGE-188] Added an export component and fix a style

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -2,3 +2,4 @@ export * from './dynamic-form';
 export * from './search-application/search-application.component';
 export * from './sidebar/sidebar.component';
 export * from './splash-screen/splash-screen.component';
+export * from './bread-crumb/bread-crumb.component';
