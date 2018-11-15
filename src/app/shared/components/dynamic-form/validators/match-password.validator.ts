20b78d092da21631c6f839c77a704d9bb414d343
Daniel <you.nightmare.00@gmail.com>
2018-11-15 02:39:45 -0400

    Â[FGE-171] Fixed identation.

--- src/app/shared/components/dynamic-form/validators/match-password.validator.ts
+++ src/app/shared/components/dynamic-form/validators/match-password.validator.ts
@@ -1,13 +1,13 @@
 import { AbstractControl } from '@angular/forms';
 
 export function ValidateMatchPassword(fieldNameToCompare: string) {
-    return (control: AbstractControl): {[key: string]: any} | null => {
-        if (control.value) {
-            const field = control.parent.controls[fieldNameToCompare];
-            if (field.value !== control.value) {
-                return { matchPassword: true };
-            }
-        }
-        return null;
-    };
+  return (control: AbstractControl): {[key: string]: any} | null => {
+    if (control.value) {
+      const field = control.parent.controls[fieldNameToCompare];
+      if (field.value !== control.value) {
+        return { matchPassword: true };
+      }
+    }
+    return null;
+  };
 }
--- src/app/shared/components/modal-confirm/modal-confirm.component.ts
+++ src/app/shared/components/modal-confirm/modal-confirm.component.ts
@@ -4,34 +4,34 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalConfirmConfig } from './modal-confirm.model';
 
 @Component({
-    selector: 'fge-modal-confirm',
-    templateUrl: './modal-confirm.component.html'
+  selector: 'fge-modal-confirm',
+  templateUrl: './modal-confirm.component.html'
 })
 export class ModalConfirmComponent {
-    @ViewChild('modalConfirmTemplate') modalContent: ElementRef;
-    @Output() readonly onsubmit: EventEmitter<any> = new EventEmitter<any>();
-    @Output() readonly oncancel: EventEmitter<any> = new EventEmitter<any>();
-    @Input() config: ModalConfirmConfig;
+  @ViewChild('modalConfirmTemplate') modalContent: ElementRef;
+  @Output() readonly onsubmit: EventEmitter<any> = new EventEmitter<any>();
+  @Output() readonly oncancel: EventEmitter<any> = new EventEmitter<any>();
+  @Input() config: ModalConfirmConfig;
 
-    constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal) { }
 
-    open(): void {
-      this.modalService.open(this.modalContent);
+  open(): void {
+    this.modalService.open(this.modalContent);
+  }
+
+  close(): void {
+    this.modalService.dismissAll();
+  }
+
+  handleSubmit() {
+    this.onsubmit.emit();
+  }
+
+  handleCancel() {
+    if (this.oncancel.observers.length > 0) {
+      this.oncancel.emit();
+    } else {
+      this.close();
     }
-
-    close(): void {
-        this.modalService.dismissAll();
-    }
-
-    handleSubmit() {
-      this.onsubmit.emit();
-    }
-
-    handleCancel() {
-      if (this.oncancel.observers.length > 0) {
-        this.oncancel.emit();
-      } else {
-        this.close();
-      }
-    }
+  }
 }
