cf07949899dbd0712c778b9f853161311ea65c77
Daniel Canqui <dcanqui@hinda.com>
2018-11-15 22:26:24 -0400

    [FGE-171] Changed index commons model.

--- src/app/core/models/commons/index.ts
+++ src/app/core/models/commons/index.ts
@@ -3,4 +3,4 @@ export * from './data-paginated.model';
 export * from './hateoas-action.model';
 export * from './link.model';
 export * from './mapped-links.model';
-export * from '../user-methods.model';
+export * from './method.model';
