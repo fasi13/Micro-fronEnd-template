ba8a891f39db2de4cd3060e00f49d42d8a945c0b
Rodrigo Mendoza <rmendoza@hinda.com>
2018-12-13 18:04:33 -0400

    [FGE-454] Added no-cache headers for IE

--- src/app/core/interceptors/unauthorized.interceptor.ts
+++ src/app/core/interceptors/unauthorized.interceptor.ts
@@ -22,6 +22,17 @@ export class UnauthorizedInterceptor implements HttpInterceptor {
   ) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+   /*
+    * Setting no-cache headers is just a quick fix to avoid caching API requests in Internet Explorer.
+    */
+    if (navigator.userAgent.indexOf('MSIE ') > -1 || navigator.userAgent.indexOf('Trident/') > -1) {
+      request = request.clone({
+        setHeaders: {
+          'Cache-Control': 'no-cache',
+          'Pragma': 'no-cache'
+        }
+      });
+    }
     return next.handle(request)
       .pipe(
         catchError((response: HttpResponse<Response>) => {
