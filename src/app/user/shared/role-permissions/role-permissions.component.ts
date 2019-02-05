02c9b1a30a8b6f5d55e6bdf93db181243f126049
Kenji Lozano <kenji.lozano.dev@gmail.com>
2019-02-05 15:23:58 -0400

    [FGE-597] Implemented expand permission item with checkbox input

--- src/app/user/shared/role-permissions/role-permissions.component.ts
+++ src/app/user/shared/role-permissions/role-permissions.component.ts
@@ -14,17 +14,155 @@ export class RolePermissionsComponent implements OnInit {
   ngOnInit() {
     this.sections = [{
       id: 1,
-      name: 'Content Management'
+      name: 'Content Management',
+      permissions: [
+        {
+          id: 1,
+          name: 'Permission 1',
+          enabled: true
+        }, {
+          id: 2,
+          name: 'Permission 2',
+          enabled: false
+        }, {
+          id: 3,
+          name: 'Permission 3',
+          enabled: false
+        }, {
+          id: 4,
+          name: 'Permission 4',
+          enabled: true
+        }, {
+          id: 5,
+          name: 'Permission 5',
+          enabled: false
+        }, {
+          id: 6,
+          name: 'Permission 6',
+          enabled: true
+        }, {
+          id: 7,
+          name: 'Permission 7',
+          enabled: false
+        }
+      ]
     }, {
       id: 2,
-      name: 'Categories Overwrite'
+      name: 'Categories Overwrite',
+      permissions: [
+        {
+          id: 1,
+          name: 'Permission A',
+          enabled: false
+        }, {
+          id: 2,
+          name: 'Permission B',
+          enabled: false
+        }, {
+          id: 3,
+          name: 'Permission C',
+          enabled: false
+        }, {
+          id: 4,
+          name: 'Permission D',
+          enabled: true
+        }, {
+          id: 5,
+          name: 'Permission E',
+          enabled: false
+        }, {
+          id: 6,
+          name: 'Permission F',
+          enabled: false
+        }, {
+          id: 7,
+          name: 'Permission G',
+          enabled: false
+        }
+      ]
     }, {
       id: 3,
-      name: 'Promotions'
+      name: 'Promotions',
+      permissions: [
+        {
+          id: 1,
+          name: 'Permission A1',
+          enabled: true
+        }, {
+          id: 2,
+          name: 'Permission B2',
+          enabled: true
+        }, {
+          id: 3,
+          name: 'Permission C3',
+          enabled: true
+        }, {
+          id: 4,
+          name: 'Permission D4',
+          enabled: false
+        }, {
+          id: 5,
+          name: 'Permission E5',
+          enabled: false
+        }, {
+          id: 6,
+          name: 'Permission F6',
+          enabled: false
+        }, {
+          id: 7,
+          name: 'Permission G7',
+          enabled: true
+        }
+      ]
     }, {
       id: 4,
-      name: 'Banners'
+      name: 'Banners',
+      permissions: [
+        {
+          id: 1,
+          name: 'Permission AA',
+          enabled: false
+        }, {
+          id: 2,
+          name: 'Permission BB',
+          enabled: false
+        }, {
+          id: 3,
+          name: 'Permission CC',
+          enabled: true
+        }, {
+          id: 4,
+          name: 'Permission DD',
+          enabled: true
+        }, {
+          id: 5,
+          name: 'Permission EE',
+          enabled: false
+        }, {
+          id: 6,
+          name: 'Permission FF',
+          enabled: true
+        }, {
+          id: 7,
+          name: 'Permission GG',
+          enabled: true
+        }
+      ]
     }];
+  }
+
+  onClickPermissionSection($event: Event, section: any): void {
+    $event.stopPropagation();
+    if (section) {
+      section.opened = !section.opened;
+    }
+  }
+
+  onClickPermissionItem($event: Event, permission: any): void {
+    $event.stopPropagation();
+    if (permission) {
+      permission.enabled = !permission.enabled;
+    }
   }
 
 }
