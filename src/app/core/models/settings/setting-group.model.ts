602ec98f5e5a193c23fcd43b777c2548a7b57404
Kenji Lozano <klozano@hinda.com>
2019-05-16 16:02:02 -0400

    [FGE-849] Removed classname from setting group model

--- src/app/core/models/settings/setting-group.model.ts
+++ src/app/core/models/settings/setting-group.model.ts
@@ -4,7 +4,6 @@ import { Setting } from './setting.model';
 export class SettingGroup extends FgeEntity {
   id: number | string;
   name: string;
-  className: string;
   description: string;
   settings: [Setting];
   _links: Link[];
