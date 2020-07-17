65ec91c9fa59d1a67daf0b852ec9e14531573c12
Mohamed HASSAIRI <MohamedHassairi@rideau.com>
2020-07-17 09:19:21 -0400

    [feature/temp/User_Story_18654__Refactor_Forge_UI_to_use_Hierarchy_API_V2] Add unit test for effects

---
+++ src/app/core/store/application/application.effects.spec.ts
@@ -0,0 +1,38 @@
+import { FetchApplicationDataSuccess, FetchApplicationPath } from './application.actions';
+import { ApplicationEffects } from './application.effects';
+import { TestBed } from '@angular/core/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { cold, hot } from 'jasmine-marbles';
+import { Observable } from 'rxjs';
+
+
+describe('My Effects', () => {
+  let effects: ApplicationEffects;
+  let actions: Observable<any>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        // any modules needed
+      ],
+      providers: [
+        ApplicationEffects,
+        provideMockActions(() => actions),
+        // other providers
+      ],
+    });
+
+    effects = TestBed.get(ApplicationEffects);
+  });
+
+  it('should work', () => {
+    const action = new FetchApplicationDataSuccess();
+    const completion = new FetchApplicationPath();
+
+    // Refer to 'Writing Marble Tests' for details on '--a-' syntax
+    actions = hot('--a-', { a: action });
+    const expected = cold('--b', { b: completion });
+
+    expect(effects.fetchApplicationDataSuccess$).toBeObservable(expected);
+  });
+});
