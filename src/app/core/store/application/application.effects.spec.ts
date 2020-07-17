72b94c54903dac62d9a188676c66ada84e7f8580
Mohamed HASSAIRI <MohamedHassairi@rideau.com>
2020-07-17 15:47:04 -0400

    [feature/temp/User_Story_18654__Refactor_Forge_UI_to_use_Hierarchy_API_V2] More unit test fixes

--- src/app/core/store/application/application.effects.spec.ts
+++ src/app/core/store/application/application.effects.spec.ts
@@ -1,41 +1,78 @@
-import { Router } from '@angular/router';
-import { FetchApplicationDataSuccess, FetchApplicationPath } from './application.actions';
+import { ApplicationService } from './../../services/application.service';
+import {
+  SearchApplication,
+  SearchApplicationSuccess,
+  ApplicationActionTypes,
+} from './application.actions';
 import { ApplicationEffects } from './application.effects';
+import { RouterModule } from '@angular/router';
+import { hot, cold } from 'jasmine-marbles';
+
+import { Observable, of } from 'rxjs';
+import { Store, StoreModule } from '@ngrx/store';
 import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
-import { cold, hot } from 'jasmine-marbles';
-import { Observable } from 'rxjs';
-import { HttpClient, HttpHandler } from '@angular/common/http';
+import * as ApplicationReducers from './application.reducers';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FetchApplicationDataSuccess, FetchApplicationPath } from '.';
+import { ApiResponse, DataPaginated, ApplicationPath } from '../../models';
 
-describe('Application Effects', () => {
+describe('application effects ', () => {
   let effects: ApplicationEffects;
   let actions: Observable<any>;
+  let store: Store<any>;
+  let originalTimeout: number;
+  let service: ApplicationService;
 
   beforeEach(() => {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
+
     TestBed.configureTestingModule({
       imports: [
-        // any modules needed
+        StoreModule.forRoot({ ...ApplicationReducers.reducer }),
+        HttpClientTestingModule,
+        RouterTestingModule,
       ],
       providers: [
         ApplicationEffects,
-        provideMockActions(() => actions),
-        // other providers
 
+        RouterModule,
+        provideMockActions(() => actions),
       ],
     });
 
     effects = TestBed.get(ApplicationEffects);
+    store = TestBed.get(Store);
+    service = TestBed.get(ApplicationService);
   });
 
-  it('should work', () => {
+  it('fetchApplicationDataSuccess should return a stream with action FetchAppPath', () => {
     const action = new FetchApplicationDataSuccess();
     const completion = new FetchApplicationPath();
 
-    // Refer to 'Writing Marble Tests' for details on '--a-' syntax
     actions = hot('--a-', { a: action });
     const expected = cold('--b', { b: completion });
+    expected.subscribe((data) => {
+      expect(data.type).toBe(ApplicationActionTypes.FETCH_APPLICATION_PATH);
+    });
+  });
 
-    expect(effects.fetchApplicationDataSuccess$).toBeObservable(expected);
+  it('search should return a stream with applicationPath result', () => {
+    spyOn(service, 'search').and.callFake(() => {});
+    const action = new SearchApplication();
+    const completion = new SearchApplicationSuccess();
+
+    actions = hot('--a-', { a: action });
+    const expected = cold('--b', { b: completion });
+    expected.subscribe((data) => {
+      expect(data.type).toBe(ApplicationActionTypes.SEARCH_APPLICATION_SUCCESS);
+    });
+  });
+
+  afterEach(function () {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
   });
 });
