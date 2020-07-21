57a173a662f2e37027f6079defa7b575709a22f2
Mohamed HASSAIRI <MohamedHassairi@rideau.com>
2020-07-21 17:37:10 -0400

    [Catan/feature/18409_Forge_Enhancements/story/18880_Support_localization_HM] Add unit tests for culture reducer

---
+++ src/app/core/store/culture/culture.reducers.spec.ts
@@ -0,0 +1,39 @@
+import { CultureAction, ReadCultureSuccessAction, ReadAvailableCulturesSuccessAction } from './culture.actions';
+import { CultureState, reducer } from './culture.reducers';
+import { initialState } from './../application/application.reducers';
+describe('CultureReducers', () => {
+
+  it('should return initial state', () => {
+    const InitialState: CultureState = {
+      currentCulture: 'en-US',
+      availableCultures: ['cul1', 'cul2']
+    };
+
+    const state = reducer(InitialState, {type: null});
+
+    expect(state).toBe(InitialState);
+  });
+
+  it('should update current culture ', () => {
+    const InitialState: CultureState = {
+      currentCulture: 'en-US',
+      availableCultures: ['cul1', 'cul2']
+    };
+
+    const state = reducer(InitialState, new ReadCultureSuccessAction({cultureCode: 'fr-CA'}));
+
+    expect(state.currentCulture).toBe('fr-CA');
+  });
+
+  it('should update available cultures', () => {
+    const InitialState: CultureState = {
+      currentCulture: 'en-US',
+      availableCultures: null
+    };
+
+    const state = reducer(InitialState, new ReadAvailableCulturesSuccessAction({availableCultures: ['fr-CA', 'en-CA']}));
+
+    expect(state.availableCultures).toEqual(['fr-CA', 'en-CA']);
+  });
+
+});
