7e231c6d06dcea6f3c2d0450f910bae654e66f73
MPlissam91867 <MemdePlissam@rideau.com>
2021-01-15 16:35:15 -0500

    correction

--- src/app/core/interceptors/timeout.interceptor.ts
+++ src/app/core/interceptors/timeout.interceptor.ts
@@ -9,7 +9,7 @@ export class TimeoutInterceptor implements HttpInterceptor {
   constructor(private timeoutService: TimeoutService, private appConfigService: AppConfigService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (this.appConfigService.config && request.url.indexOf(this.appConfigService.config.apis[0].url)) {
+    if (this.appConfigService.config && request.url.indexOf(this.appConfigService.config.apis[0].url) >= 0) {
       this.timeoutService.requestReceived();
     }
 
