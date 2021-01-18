1b9a7538d4795fbe87cf3587db2c01955aaf44d8
RIDEAUINC\sCoutu <samuelcoutu@rideau.com>
2021-01-18 13:09:21 -0500

    [feature/green/Bug_25817___Communication_Management_Section]_Content_Groups_page_reloaded_before_moving_to_Communications_page] Bug 25817: [Communication Management Section] Content Groups page reloaded before moving to Communications page

--- src/app/communication/_guards/communication-roles.guard.spec.ts
+++ src/app/communication/_guards/communication-roles.guard.spec.ts
@@ -1,24 +1,17 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { State, TestInitialState } from '@forge/core';
-import { Store } from '@ngrx/store';
-import { provideMockStore } from '@ngrx/store/testing';
 import { CommunicationRolesGuard } from './communication-roles.guard';
 
 describe('CommunicationRolesGuard', () => {
   let guard: CommunicationRolesGuard;
-  let store: Store<State>;
-  const initialState: State = TestInitialState;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [provideMockStore({ initialState })],
+      providers: [],
       declarations: [],
       imports: [],
       schemas: []
     });
-    store = TestBed.get(Store);
-    guard = new CommunicationRolesGuard(store);
+    guard = new CommunicationRolesGuard();
   });
 
   it('should be created', (done) => {
--- src/app/communication/_guards/communication-roles.guard.ts
+++ src/app/communication/_guards/communication-roles.guard.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
-import { Store } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
 import { switchMap, catchError } from 'rxjs/operators';
-import { State, FetchContentGroups } from '@forge/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommunicationRolesGuard implements CanActivate {
-
-  constructor(
-    private store: Store<State>
-  ) {}
 
   canActivate(): Observable<boolean> {
     return this.getFromStoreOrAPI()
@@ -23,13 +17,6 @@ export class CommunicationRolesGuard implements CanActivate {
   }
 
   private getFromStoreOrAPI(): Observable<any> {
-    /**
-     * @TODO Check if users has been loaded or there is a list of them in store,
-     *  but in order to get that completed we should refactor users listing component
-     *  to have the pagination, sorting variables in redux/store, then we can implement that
-     *  logic to perform the action to fetch users.
-     */
-    this.store.dispatch(new FetchContentGroups());
     return of(true);
   }
 }
