509b83c8a1f531a03cc525dbbf1b45dc0b2364c0
RIDEAUINC\sCoutu <samuelcoutu@rideau.com>
2021-02-04 15:30:37 -0500

    [feature/green/Bug_26285___Edit_Communication]_The_user's_work_is_lost_when_clicking_on_browser's_back_button] fixed code coverage

--- src/app/service/start-service.component.spec.ts
+++ src/app/service/start-service.component.spec.ts
@@ -1,12 +1,12 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { provideMockStore } from '@ngrx/store/testing';
+import { serviceMatcherFunction, ServiceModule } from './service.module';
 import { StartServiceComponent } from './start-service.component';
 
-describe('StartServiceComponent', () => {
+fdescribe('StartServiceComponent', () => {
   let fixture: ComponentFixture<StartServiceComponent>;
   let component: StartServiceComponent;
   let router: Router;
@@ -14,8 +14,8 @@ describe('StartServiceComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [provideMockStore({ })],
-      declarations: [StartServiceComponent],
-      imports: [RouterTestingModule.withRoutes([])],
+      declarations: [],
+      imports: [RouterTestingModule.withRoutes([]), ServiceModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     });
     fixture = TestBed.createComponent(StartServiceComponent);
@@ -27,6 +27,9 @@ describe('StartServiceComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+    var urlParam = [];
+    let match = serviceMatcherFunction(urlParam)
+    expect(match.consumed).toBe(urlParam);
   });
 
   afterEach(() => {
