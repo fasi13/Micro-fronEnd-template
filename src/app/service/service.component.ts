80b379c1fce560ee9ee45a741dfd5cac33038f70
RIDEAUINC\sCoutu <samuelcoutu@rideau.com>
2021-02-10 17:30:02 -0500

    [feature/green/Bug_26285___Edit_Communication]_The_user's_work_is_lost_when_clicking_on_browser's_back_button] added user identity

--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { getAuthenticatedUser, State } from '@forge/core-store';
 import { AppConfigService } from '../app-config.service';
+import { User } from '../core/models';
 @Component({
   selector: 'fge-service',
   templateUrl: './service.component.html'
 })
 export class ServiceComponent implements OnInit  {
   componentHtml: SafeHtml;
-  constructor(private route: ActivatedRoute, private router: Router, private sanitizer: DomSanitizer, private appConfigService: AppConfigService) {}
+  user: User;
+  constructor(private route: ActivatedRoute, private router: Router, private sanitizer: DomSanitizer, private appConfigService: AppConfigService, private store: Store<State>) {
+    this.store.select(getAuthenticatedUser)
+    .subscribe((user: User) => {
+      this.user = user;
+    });
+  }
   ngOnInit() {
     let applicationId = Number(this.route.snapshot.params['tenantId']);
     let baseHref = this.router.url.split('/').slice(0, 5).join("/");
@@ -16,7 +25,7 @@ export class ServiceComponent implements OnInit  {
     // we get the config id, by id to prevent XSS, so we don't inject the string directly in the DOM from the url
     let config = this.appConfigService.config.services.find((x) => x.id == configId);
     let serviceId = config.id;
-    let html = `<e2e-${serviceId}-management-app application-id="${applicationId}" base-href="${baseHref}"></e2e-${serviceId}-management-app>`
+    let html = `<e2e-${serviceId}-management-app application-id="${applicationId}" base-href="${baseHref}" user-identity="${this.user.id}"></e2e-${serviceId}-management-app>`
     this.componentHtml = this.sanitizer.bypassSecurityTrustHtml(html);
   }
 }
