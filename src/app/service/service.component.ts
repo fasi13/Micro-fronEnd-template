b783f426b2abc2d92dbb3f693adb9a669c9ea693
Mohamed HASSAIRI <MohamedHassairi@rideau.com>
2021-03-15 15:31:17 -0400

    [bugFix-27398] version 25

--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -1,3 +1,4 @@
+import { UserService } from './../core/services/user.service';
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -12,20 +13,20 @@ import { User } from '../core/models';
 export class ServiceComponent implements OnInit  {
   componentHtml: SafeHtml;
   user: User;
-  constructor(private route: ActivatedRoute, private router: Router, private sanitizer: DomSanitizer, private appConfigService: AppConfigService, private store: Store<State>) {
+  constructor(private route: ActivatedRoute, private router: Router, private sanitizer: DomSanitizer, private appConfigService: AppConfigService, private store: Store<State>, private userService: UserService) {
     this.store.select(getAuthenticatedUser)
     .subscribe((user: User) => {
       this.user = user;
     });
   }
   ngOnInit() {
-    let applicationId = Number(this.route.snapshot.params['tenantId']);
-    let baseHref = this.router.url.split('/').slice(0, 5).join("/");
-    let configId = this.router.url.split('/')[4];
+    const applicationId = Number(this.route.snapshot.params['tenantId']);
+    const baseHref = this.router.url.split('/').slice(0, 5).join('/');
+    const configId = this.router.url.split('/')[4];
     // we get the config id, by id to prevent XSS, so we don't inject the string directly in the DOM from the url
-    let config = this.appConfigService.config.services.find((x) => x.id == configId);
-    let serviceId = config.id;
-    let html = `<e2e-${serviceId}-management-app application-id="${applicationId}" base-href="${baseHref}" user-identity="${this.user.id}"></e2e-${serviceId}-management-app>`
+    const config = this.appConfigService.config.services.find((x) => x.id === configId);
+    const serviceId = config.id;
+    const html = `<e2e-${serviceId}-management-app application-id="${applicationId}" base-href="${baseHref}" user-identity="${this.user.id}" user-authorization="Token ${this.userService.getToken()}"></e2e-${serviceId}-management-app>`;
     this.componentHtml = this.sanitizer.bypassSecurityTrustHtml(html);
   }
 }
