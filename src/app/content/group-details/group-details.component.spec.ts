9f14a270156401e28893d72f8f3b4048a91846ad
Mohamed HASSAIRI <MohamedHassairi@rideau.com>
2021-04-22 19:11:54 -0400

    [feature/green/Bug_27111___Content_Management]_Performance]_Slow_creation_of_new_content] add unit test for group details page

--- src/app/content/group-details/group-details.component.spec.ts
+++ src/app/content/group-details/group-details.component.spec.ts
@@ -1,3 +1,5 @@
+import { ApplicationContent } from './../../core/models/application/application-content.model';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { CoreModule } from '../../core/core.module';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
@@ -19,19 +21,42 @@ import { ContentHtmlEditorComponent } from '../content-html-editor/content-html-
 import { GroupsListingComponent } from '../groups-listing/groups-listing.component';
 import { ContentEditorModalComponent } from '../shared/content-editor-modal/content-editor-modal.component';
 import { ContentFormModalComponent } from '../shared/content-form-modal/content-form-modal.component';
-import { ContentInlineEditorComponent, ShowLinkPipe } from '../shared/content-inline-editor/content-inline-editor.component';
-import { ContentTypeColorComponent, ContentTypeTextComponent, ContentTypeHtmlComponent, ContentTypeImageComponent, ContentTypeLogoComponent, ContentTypeDocumentComponent } from '../shared/content-types';
+import {
+  ContentInlineEditorComponent,
+  ShowLinkPipe,
+} from '../shared/content-inline-editor/content-inline-editor.component';
+import {
+  ContentTypeColorComponent,
+  ContentTypeTextComponent,
+  ContentTypeHtmlComponent,
+  ContentTypeImageComponent,
+  ContentTypeLogoComponent,
+  ContentTypeDocumentComponent,
+} from '../shared/content-types';
 import { GroupFormModalComponent } from '../shared/group-form-modal/group-form-modal.component';
+import { of } from 'rxjs';
+
+export class MockNgbModalRef {
+  constructor(private content: ApplicationContent) {}
+  componentInstance = {
+    groupId: undefined,
+    group: undefined,
+  };
+  result: Promise<ApplicationContent> = new Promise((resolve, reject) =>
+    resolve(this.content)
+  );
+}
 
 describe('GroupDetailsComponent', () => {
   let fixture: ComponentFixture<GroupDetailsComponent>;
   let component: GroupDetailsComponent;
   let store: MockStore<State>;
-  let modalService: FgeModalService;
-  const  initialState: State = TestInitialState;
+  let fgeModalService: FgeModalService;
+  let modalService: NgbModal;
+  const initialState: State = TestInitialState;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ provideMockStore({ initialState }), FgeModalService],
+      providers: [provideMockStore({ initialState }), FgeModalService],
       declarations: [
         GroupDetailsComponent,
         GroupsListingComponent,
@@ -47,7 +72,7 @@ describe('GroupDetailsComponent', () => {
         ContentHtmlEditorComponent,
         ContentInlineEditorComponent,
         GroupNotFoundComponent,
-        ShowLinkPipe
+        ShowLinkPipe,
       ],
       imports: [
         NotifierModule,
@@ -58,9 +83,9 @@ describe('GroupDetailsComponent', () => {
         NgBootstrapModule,
         RouterTestingModule,
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
       ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     });
 
     store = TestBed.get(Store);
@@ -68,16 +93,55 @@ describe('GroupDetailsComponent', () => {
     fixture = TestBed.createComponent(GroupDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-    modalService = TestBed.get(FgeModalService);
+    fgeModalService = TestBed.get(FgeModalService);
+    modalService = TestBed.get(NgbModal);
+    component.currentGroup = { id: 0, name: '', _links: [], content: null };
+    component.listContents = [
+      { dataType: { name: 'Document', type: '' }, name: 'name5', version: 0 },
+    ];
+    spyOn(fgeModalService, 'registerModal').and.callFake(() => {});
   });
 
   fit('should be created', () => {
     expect(component).toBeTruthy();
   });
 
+  const testCases = [
+    {ex: 'lower datatype', displayAsList: true, name: 'name5', dataTypeName: 'Color Picker', expectPosition: 0 },
+    {ex: 'upper datatype', displayAsList: true,  name: 'name1', dataTypeName: 'Image', expectPosition: 1 },
+    {ex: 'same datatype & lower name', displayAsList: true, name: 'name4', dataTypeName: 'Document', expectPosition: 0 },
+    {ex: 'same datatype & upper name', displayAsList: true,  name: 'name6', dataTypeName: 'Document', expectPosition: 1 },
+
+    {ex: 'lower datatype', displayAsList: false, name: 'name5', dataTypeName: 'Color Picker', expectPosition: 0 },
+    {ex: 'upper datatype', displayAsList: false,  name: 'name1', dataTypeName: 'Image', expectPosition: 1 },
+    {ex: 'same datatype & lower name', displayAsList: false, name: 'name4', dataTypeName: 'Document', expectPosition: 0 },
+    {ex: 'same datatype & upper name', displayAsList: false,  name: 'name6', dataTypeName: 'Document', expectPosition: 1 },
+
+
+  ];
+  testCases.forEach((testCase) => {
+    fit(`when new ${testCase.displayAsList ? 'list' : 'no list'} content is ${testCase.ex} should add to list in position ${testCase.expectPosition}` , (done) => {
+      const content = {
+        displayAsList: testCase.displayAsList,
+        dataType: { name: testCase.dataTypeName, type: '' },
+        name: testCase.name,
+        version: 0,
+      };
+
+      const mockModalRef = new MockNgbModalRef( content);
+      spyOn(modalService, 'open').and.returnValue(mockModalRef);
+
+      component.openContentForm().then(() => {
+        expect(component.listContents.length).toBe(2);
+
+        const position = component.listContents.indexOf(content);
+        expect(position).toBe(testCase.expectPosition);
+        done();
+      });
+    });
+  });
+
   afterEach(() => {
     fixture.destroy();
   });
 });
-
-
