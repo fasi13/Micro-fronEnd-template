f8edaece2fd2db6cdae0662ac3d5e0c76273373d
derese getachew <deresegetachew@gmail.com>
2021-06-16 20:28:57 +0300

    husky hook functionality test

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ function App(): JSX.Element {
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
 					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
