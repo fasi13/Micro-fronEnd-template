4e612d61343c5f2666617ac5b3efca369252dd51
derese getachew <deresegetachew@gmail.com>
2021-06-22 14:56:29 +0300

    fixing test coverage issues

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
 import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
-	render(<App />);
-	screen.getByText(/learn react/i);
+test('loads and displays App', () => {
+	const { container, getByTestId, getByText, queryByTestId } = render(<App />);
+	const logo = getByTestId('logo', { exact: true });
+	const p = getByTestId('message');
+
+	getByText('get started', { exact: false });
+	expect(logo).toBeInTheDocument();
+	expect(p).toBeInTheDocument();
+	expect(queryByTestId('learn-link')).toBeInTheDocument();
 });
