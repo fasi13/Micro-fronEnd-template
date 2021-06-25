59834a67dbd4fed95bcbe992cb1d2a4d2d8f1073
derese getachew <deresegetachew@gmail.com>
2021-06-25 14:43:34 +0300

    restructure folder structure

---
+++ src/pages/app/App.test.tsx
@@ -0,0 +1,21 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import React from 'react';
+import App from './App';
+
+describe('App', () => {
+	test('renders App component', () => {
+		render(<App />);
+	});
+
+	test('loads and displays App', () => {
+		const { getByTestId, getByText, queryByTestId } = render(<App />);
+		const logo = getByTestId('logo', { exact: true });
+		const p = getByTestId('message');
+
+		getByText('get started', { exact: false });
+		expect(logo).toBeInTheDocument();
+		expect(p).toBeInTheDocument();
+		expect(queryByTestId('learn-link')).toBeInTheDocument();
+	});
+});
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface Hierarchy {
+	name: string;
+}
+
+export interface MfeOption {
+	description: string;
+	logo: string;
+	order: number;
+}
