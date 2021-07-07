9cff6a18cf68320996c8c4e2ace240d4600b49e4
Tedo <getitdonetedo@gmail.com>
2021-07-07 08:24:06 -0700

    search-ui integrated to sidebar

--- src/common/components/search-application.tsx
+++ src/common/components/search-application.tsx
@@ -1,14 +1,33 @@
-import { List, ListItem, ListItemText, Typography } from '@material-ui/core';
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { createStyles, ListItem, ListItemText, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
-import { useSearchStore } from '../../state';
 import { ApplicationPath } from '../../types';
 
-export const SearchApplication = () => {
-	const { searchLoading, searchData } = useSearchStore();
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      borderBottom: "1px solid #dcdcdc",
+    }
+  }),
+);
+
+
+interface SearchApplicationProps {
+  item: ApplicationPath;
+}
+
+export const SearchApplication = (props: SearchApplicationProps) => {
+
+  const { item } = props;
+  const classes = useStyles();
 
 	const getApplicationName = ({ path }: ApplicationPath): string =>
 		path[path.length - 1].name;
+
+	const getApplicationId = ({ path }: ApplicationPath): string =>
+		path[path.length - 1].id.toString();
 
 	const getApplicationValue = ({ path }: ApplicationPath): string =>
 		path[path.length - 1].value;
@@ -29,35 +48,91 @@ export const SearchApplication = () => {
 	const getApplicationLink = ({ path }: ApplicationPath): string =>
 		`tenant/${path[path.length - 1].id}/service/content`;
 
+
 	return (
-		<List>
-			{searchLoading ? (
-				<p>Loading...</p>
-			) : (
-				searchData.map(item => (
-					<Router>
-						{' '}
-						<Link to={getApplicationLink(item)}>
-							<ListItem>
-								<ListItemText
-									primary={getApplicationName(item)}
-									secondary={
-										<>
-											<Typography component="span" variant="body2">
-												{getApplicationValue(item)}
-											</Typography>{' '}
-											<br />
-											<Typography component="span" variant="body2">
-												{getApplicationPath(item)}
-											</Typography>
-										</>
-									}
-								/>
-							</ListItem>
-						</Link>
-					</Router>
-				))
-			)}
-		</List>
+    <Router>
+      <Link to={getApplicationLink(item)} key={getApplicationId(item)} >
+        <ListItem className={classes.root}>
+          <ListItemText
+            primary={getApplicationName(item)}
+            secondary={
+              <>
+                <Typography component="span" variant="body2">
+                  {getApplicationValue(item)}
+                </Typography>{' '}
+                <br />
+                <Typography component="span" variant="body2">
+                  {getApplicationPath(item)}
+                </Typography>
+              </>
+            }
+          />
+        </ListItem>
+      </Link>
+    </Router>
 	);
 };
+
+// import { List, ListItem, ListItemText, Typography } from '@material-ui/core';
+// import React from 'react';
+// import { BrowserRouter as Router, Link } from 'react-router-dom';
+// import { useSearchStore } from '../../state';
+// import { ApplicationPath } from '../../types';
+
+// export const SearchApplication = () => {
+// 	const { searchLoading, searchData } = useSearchStore();
+
+// 	const getApplicationName = ({ path }: ApplicationPath): string =>
+// 		path[path.length - 1].name;
+
+// 	const getApplicationValue = ({ path }: ApplicationPath): string =>
+// 		path[path.length - 1].value;
+
+// 	const getApplicationPath = (application: ApplicationPath): string => {
+// 		const appPath = application.path;
+// 		let strPath = '';
+// 		appPath.slice(0, appPath.length - 1).forEach((element, index, array) => {
+// 			if (element) {
+// 				const separator = index === array.length - 1 ? '' : '>';
+// 				const elementId = +element.value > -1 ? `(${element.value})` : '';
+// 				strPath += `${element.value}${elementId}${separator}`;
+// 			}
+// 		});
+// 		return strPath;
+// 	};
+
+// 	const getApplicationLink = ({ path }: ApplicationPath): string =>
+// 		`tenant/${path[path.length - 1].id}/service/content`;
+
+// 	return (
+// 		<List>
+// 			{searchLoading ? (
+// 				<p>Loading...</p>
+// 			) : (
+// 				searchData.map(item => (
+// 					<Router>
+// 						{' '}
+// 						<Link to={getApplicationLink(item)}>
+// 							<ListItem>
+// 								<ListItemText
+// 									primary={getApplicationName(item)}
+// 									secondary={
+// 										<>
+// 											<Typography component="span" variant="body2">
+// 												{getApplicationValue(item)}
+// 											</Typography>{' '}
+// 											<br />
+// 											<Typography component="span" variant="body2">
+// 												{getApplicationPath(item)}
+// 											</Typography>
+// 										</>
+// 									}
+// 								/>
+// 							</ListItem>
+// 						</Link>
+// 					</Router>
+// 				))
+// 			)}
+// 		</List>
+// 	);
+// };
--- src/common/components/sidebar/sidebar.tsx
+++ src/common/components/sidebar/sidebar.tsx
@@ -1,10 +1,48 @@
-import React, { useEffect } from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import { CircularProgress, createStyles, List, makeStyles, TextField } from '@material-ui/core';
+import { Autocomplete } from '@material-ui/lab';
+import React, { useEffect, useState } from 'react';
 import { Rnd } from 'react-rnd';
-import { HierarchyTree } from '..';
-import { detachStore, useHierarchyStore } from '../../../state';
-import { TreeView } from '../../../types';
+import { HierarchyTree, SearchApplication } from '..';
+import { detachStore, useHierarchyStore, useSearchStore } from '../../../state';
+import { ApplicationPath, TreeView } from '../../../types';
+
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    rootList: {
+      // zIndex: theme.zIndex.tooltip,
+      margin: -16,
+      background: "white",
+      width: 320,
+      "& :hover": {
+        background: "#d3d3d3"
+      }
+    },
+    searchContainer: {
+      //  zIndex: theme.zIndex.tooltip,
+    },
+    // searchList: {
+    //   display: "flex",
+    //   justifyContent: "center",
+    //   zIndex: theme.zIndex.tooltip,
+    // }
+  }),
+);
+
 
 const SidebarContent = () => {
+
+  const classes = useStyles();
+
+  // searchLoading,
+	const { setSearchLoading, searchApplication, searchData } = useSearchStore();
+
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const [open, setOpen] = React.useState(false);
+  const searchLoading = open && searchData.length === 0;
+
 	const {
 		initializeHierarchyState,
 		hierarchyData,
@@ -17,73 +55,143 @@ const SidebarContent = () => {
 		editApplicationGroup,
 	} = useHierarchyStore();
 
+
 	useEffect(() => {
 		initializeHierarchyState();
 		setLoading(true);
 		getUserApplication();
 	}, []);
 
+  useEffect(() => {
+		const handler = setTimeout(() => {
+			if (inputValue) {
+				setSearchLoading(true);
+				searchApplication(inputValue);
+			}
+		}, 1000);
+		return () => {
+			clearTimeout(handler);
+		};
+	}, [inputValue]);
+
+  const getApplicationName = ({ path }: ApplicationPath): string  =>
+		path[path.length - 1].name;
+
+  const searchElement = (keyword: string) =>
+  keyword.length < 3 ? null : setInputValue(keyword);
+
 	return (
 		<>
 			<div className="w-full overflow-visible bg-grayblue">
-				<HierarchyTree
-					onSelect={() => {
-						console.log('hi');
-					}}
-					onToggle={async (
-						item: TreeView,
-						nodeId: number,
-						nodePath: number[],
-						cb: () => void,
-					) => {
-						await getHierarchyChildData(item, nodeId, nodePath);
-						cb();
-					}}
-					onAddGroup={async (
-						item: TreeView,
-						nodeId: number,
-						nodePath: number[],
-						name: string,
-						cb: (err: any) => void,
-					) => {
-						await createApplicationGroup(item, nodeId, nodePath, name);
-						cb(null);
-					}}
-					onAddApplication={async (
-						item: TreeView,
-						nodeId: number,
-						nodePath: number[],
-						name: string,
-						value: string,
-						cb: (err: any) => void,
-					) => {
-						await createApplication(item, nodeId, nodePath, name, value);
-						cb(null);
-					}}
-					onEditApplication={async (
-						item: TreeView,
-						nodeId: number,
-						nodePath: number[],
-						name: string,
-						value: string,
-						cb: (err: any) => void,
-					) => {
-						await editApplication(item, nodeId, nodePath, name, value);
-						cb(null);
-					}}
-					onEditGroup={async (
-						item: TreeView,
-						nodeId: number,
-						nodePath: number[],
-						name: string,
-						cb: (err: any) => void,
-					) => {
-						await editApplicationGroup(item, nodeId, nodePath, name);
-						cb(null);
-					}}
-					data={hierarchyData}
-					expandNodesAtLevel={0}
-				/>
+        <>
+          <div className="flex justify-center">
+             <Autocomplete
+              open={open}
+              onOpen={() => {
+                setOpen(true);
+              }}
+              onClose={() => {
+                setOpen(false);
+              }}
+              //  className={classes.searchContainer}
+              freeSolo
+              id="combo-box-demo"
+              style={{ width: 320, backgroundColor: "#fff", zIndex: 999999, }}
+              // getOptionSelected={(option, value) => option.name === value.name}
+              options={searchData}
+              getOptionLabel = {(x) =>  getApplicationName(x)}
+              autoComplete
+              fullWidth
+              loading={searchLoading}
+              renderInput={params => (
+                <TextField
+                  {...params}
+                  onChange={e => searchElement(e.target.value.toString())}
+                  label="Search application..."
+                  variant="outlined"
+                  fullWidth
+                  InputProps={{
+                    ...params.InputProps,
+                    endAdornment: (
+                      <>
+                        {searchLoading ? <CircularProgress color="inherit" size={20} /> : null}
+                        {params.InputProps.endAdornment}
+                      </>
+                    ),
+                  }}
+                />
+              )}
+             renderOption={() =>
+                searchData.length !== 0 ?
+                  <div>
+                    <List className={classes.rootList}>
+                      {searchData.map((item: ApplicationPath) => (
+                          <SearchApplication item={item} />
+                      ))}
+                    </List>
+                  </div> : <p>No results found! </p>
+              }
+            />
+          </div>
+          <HierarchyTree
+            onSelect={() => {
+              console.log('hi');
+            }}
+            onToggle={async (
+              item: TreeView,
+              nodeId: number,
+              nodePath: number[],
+              cb: () => void,
+            ) => {
+              await getHierarchyChildData(item, nodeId, nodePath);
+              cb();
+            }}
+            onAddGroup={async (
+              item: TreeView,
+              nodeId: number,
+              nodePath: number[],
+              name: string,
+              cb: (err: any) => void,
+            ) => {
+              await createApplicationGroup(item, nodeId, nodePath, name);
+              cb(null);
+            }}
+            onAddApplication={async (
+              item: TreeView,
+              nodeId: number,
+              nodePath: number[],
+              name: string,
+              value: string,
+              cb: (err: any) => void,
+            ) => {
+              await createApplication(item, nodeId, nodePath, name, value);
+              cb(null);
+            }}
+            onEditApplication={async (
+              item: TreeView,
+              nodeId: number,
+              nodePath: number[],
+              name: string,
+              value: string,
+              cb: (err: any) => void,
+            ) => {
+              await editApplication(item, nodeId, nodePath, name, value);
+              cb(null);
+            }}
+            onEditGroup={async (
+              item: TreeView,
+              nodeId: number,
+              nodePath: number[],
+              name: string,
+              cb: (err: any) => void,
+            ) => {
+              await editApplicationGroup(item, nodeId, nodePath, name);
+              cb(null);
+            }}
+            data={hierarchyData}
+            expandNodesAtLevel={0}
+          />
+        </>
 			</div>
 		</>
 	);
@@ -95,7 +203,8 @@ export const Sidebar = () => {
 		alignItems: 'start',
 		justifyContent: 'center',
 		background: '#31506A',
-		zIndex: 99999,
+		// zIndex: 99999,
+		zIndex: 1300,
 		top: '35px !important',
 		left: '35px !important',
 		boxShadow: 'rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px',
