3890dd4a3e20b68a3ac64b1269b647ebec53a394
Eyob Samuel <eysamuel@excellerentsolutions.com>
2021-08-05 19:39:17 +0300

    snapshot file added from window os

--- src/common/components/hierarchyTree/__test__/nodeEditor.test.tsx
+++ src/common/components/hierarchyTree/__test__/nodeEditor.test.tsx
@@ -89,7 +89,7 @@ describe('nodeEditor', () => {
 		const submitHandler = jest.fn();
 		const onSetNodeErr = jest.fn();
 
-		const { getByTestId, debug } = render(
+		const { getByTestId } = render(
 			<NodeEditor
 				key={`node_editor_${dummyTreeView[0].id}`}
 				onClose={() => {
@@ -104,7 +104,7 @@ describe('nodeEditor', () => {
 				clearError={onSetNodeErr()}
 			/>,
 		);
-		debug();
+
 		const input = getByTestId(nodeEditorInputStr) as HTMLInputElement;
 		fireEvent.change(input, {
 			target: { value: 'test' },
