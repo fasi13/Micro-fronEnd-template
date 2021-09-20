27325405a6fc0bc636a55ac13b509cd3ed7dd5dd
Fasile Endalkachew <fendalkachew@excellerentsolutions.com>
2021-09-20 12:04:00 +0300

    adding rules

--- src/pages/app/app.tsx
+++ src/pages/app/app.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-const App = () => (
-	<div className="App">
-		<div>Micro FrontEnd Template</div>
-	</div>
-);
+const App = () => <div>Micro FrontEnd Template</div>;
+
 export default App;
